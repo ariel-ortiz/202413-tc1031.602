bf50a830d1cabdc13b1b6d7cf00b9409e4e71f35
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-08-14 22:57:21 +0000

    Add code for first version of DinoSet class

---
+++ 1. DinoSet/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "dinoset.h"
+
+int main()
+{
+    DinoId x, y;
+    x = DinoId::velociraptor;
+    y = DinoId::triceratops;
+    x = static_cast<DinoId>(1);
+    int i = static_cast<int>(y);
+
+    DinoSet a;
+    a.add(DinoId::velociraptor);
+    a.add(DinoId::tyrannosaurus);
+
+    DinoSet b;
+    b.add(DinoId::tyrannosaurus);
+    b.add(DinoId::triceratops);
+    b.add(DinoId::pteranodon);
+    b.add(DinoId::tyrannosaurus);
+
+    std::cout << "a = " << a.to_string() << "\n";
+    std::cout << "b = " << b.to_string() << "\n";
+    std::cout << "a.contains(DinoId::tyrannosaurus) = " << a.contains(DinoId::tyrannosaurus) << "\n";
+    std::cout << "a.contains(DinoId::procompsognathus) = " << a.contains(DinoId::procompsognathus) << "\n";
+
+    return 0;
+}
