1a0024d0661b177af4a103d56f24ca5d3f08bfee
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-11-27 23:02:42 +0000

    Add code to complete TreeSet implementation

--- 10. Binary Search Trees/treeset/main.cpp
+++ 10. Binary Search Trees/treeset/main.cpp
@@ -22,11 +22,17 @@ int main()
     a.add(7);
     std::cout << "a.size() = " << a.size() << "\n";
 
-    TreeSet<int> b {7, 3, 5, 10, 2, 8, 12};
+    TreeSet<int> b {7, 3, 5, 10, 2, 8};
     std::cout << "b.size() = " << b.size() << "\n";
     b.inorder(print_int);
     std::cout << "\n";
     b.inorder(add_ints);
     std::cout << total << "\n";
+    b.levelorder(print_int);
+    std::cout << "\n";
+    std::cout << "b.height() = " << b.height() << "\n";
+    std::cout << "b.is_full() = " << b.is_full() << "\n";
+    std::cout << "b.leaf_count() = " << b.leaf_count() << "\n";
+    std::cout << "b.is_perfect() = " << b.is_perfect() << "\n";
     return 0;
 }
